82d6944c4a0c012cd40ce999f53abbdc786bae41
Pierre-Henri Dupouy <pidupouy@emi.u-bordeaux1.fr>
2019-12-06 02:12:11 +0100

    modification

--- Web/bdd.php
+++ Web/bdd.php
@@ -1,17 +1,13 @@
 
 <?php
 
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-    foreach($dbh->query('SELECT * from FOO') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
+if(!file_exists("users.json"))
+    file_put_contents("users.json", "{}");
 
+$users = json_decode(file_get_contents("users.json"), true);
+
+$users["THomas"] = array("password" => "encryptedpasscord", "age" => 18, "mail" => "pdgdegoogle@gmail.com", "school" => "Univ BDX", "formation" => "MISIPCG", "state" => 1,
+ "job" => true, "jobIncome" => 100, "monthIncome" => 500, 
+"monthExpends" => ["feed" => 10, "rent" => 300,"moving" => 20, "other" => 10]);
+var_dump($users);
 ?>
